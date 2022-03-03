9446e80904a54111ce3cb45473dc623b4a0c97fe
wormholecowboy <bgildeab@gmail.com>
2022-03-02 23:45:08 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <img
+        src="http://github.com/wormholecowboy.png"
+        height={30}
+        alt="something describing the thing"
+      />
+      <p>
+        We eat {props.adjective} babies and sometimes even woke people. Would
+        you care to donate?
+      </p>
+      <ul style={{ textAlign: 'center', listStylePosition: 'inside' }}>
+        {props.dishes.map((dish) => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
+
+const dishes = ['Mac & cheese', 'taters', 'perogis'];
+
+const dishObj = dishes.map((dish, i) => ({ id: i, title: dish }));
+console.log(dishObj);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Shannon" />
+      <Main adjective="savory" dishes={dishObj} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
