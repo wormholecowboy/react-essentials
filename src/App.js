dfc57d4d1ea02e1ae31479e19137107dfa834843
wormholecowboy <bgildeab@gmail.com>
2022-03-03 23:15:32 -0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,54 +1,16 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
-}
-
-function Main(props) {
-  return (
-    <section>
-      <img
-        src="http://github.com/wormholecowboy.png"
-        height={30}
-        alt="something describing the thing"
-      />
-      <p>
-        We eat {props.adjective} babies and sometimes even woke people. Would
-        you care to donate?
-      </p>
-      <ul style={{ textAlign: 'center', listStylePosition: 'inside' }}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
-}
-
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-
-const dishes = ['Mac & cheese', 'taters', 'perogis'];
-
-const dishObj = dishes.map((dish, i) => ({ id: i, title: dish }));
-console.log(dishObj);
-
 function App() {
+  const [emotion, setEmotion] = useState('happy');
+  console.table(emotion);
   return (
-    <div className="App">
-      <Header name="Shannon" />
-      <Main adjective="savory" dishes={dishObj} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
+    <>
+      <h1>Current emotion is {emotion}.</h1>
+      <button onClick={() => setEmotion('fascinated')}>Fascinate Me</button>
+      <button onClick={() => setEmotion('constipated')}>Constipate Me</button>
+      <button onClick={() => setEmotion('sublime')}>Sublime Me</button>
+    </>
   );
 }
 
